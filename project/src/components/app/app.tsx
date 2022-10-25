e4554085f279efae7a3e73f01e14e525561a0ea0
Oleh Parkhomenko <oleg.parkhom@gmail.com>
2022-10-25 06:20:46 -0600

    Получает данные из props и передаёт их в компонент главной страницы приложения

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,8 +1,12 @@
 import MainPage from '../../pages/main-page/main-page';
 
-function App(): JSX.Element {
+type AppProps = {
+  rentalsNum: number;
+}
+
+function App({ rentalsNum }: AppProps): JSX.Element {
   return (
-    <MainPage />
+    <MainPage rentalsNum={rentalsNum} />
   );
 }
 
