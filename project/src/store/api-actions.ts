f43fc38a3cfdddb28b1baf483c135463b681ef4f
Oleh Parkhomenko <oleg.parkhom@gmail.com>
2022-11-28 01:55:30 -0700

    Описывает новое асинхронное действие для проверки прохождения авторизации

--- project/src/store/api-actions.ts
+++ project/src/store/api-actions.ts
@@ -2,8 +2,8 @@ import { AxiosInstance } from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import { AppDispatch, State } from '../types/state.js';
 import { Hotel } from '../types/hotel';
-import { loadOffers, setOffersDataLoadingStatus } from './action';
-import { APIRoute } from '../const';
+import { loadOffers, requireAuthorization, setOffersDataLoadingStatus } from './action';
+import { APIRoute, AuthorizationStatus } from '../const';
 
 export const fetchOfferAction = createAsyncThunk<void, undefined, {
   dispatch: AppDispatch;
@@ -16,5 +16,21 @@ export const fetchOfferAction = createAsyncThunk<void, undefined, {
     const { data } = await api.get<Hotel[]>(APIRoute.Offers);
     dispatch(setOffersDataLoadingStatus(false));
     dispatch(loadOffers(data));
+  },
+);
+
+export const checkAuthAction = createAsyncThunk<void, undefined, {
+  dispatch: AppDispatch;
+  state: State;
+  extra: AxiosInstance;
+}>(
+  'user/checkAuth',
+  async (_arg, { dispatch, extra: api }) => {
+    try {
+      await api.get(APIRoute.Login);
+      dispatch(requireAuthorization(AuthorizationStatus.Auth));
+    } catch {
+      dispatch(requireAuthorization(AuthorizationStatus.NoAuth));
+    }
   },
 );
