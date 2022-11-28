709c2d28e4712ee4533acfd5c0bbf821eff0ed4c
Oleh Parkhomenko <oleg.parkhom@gmail.com>
2022-11-28 05:14:42 -0700

    Добавит отображение в шапке страницы ссылки для перехода на страницу ввода логина и пароля или информацию о пользователе и кнопку для выхода в зависимости от авторизации

--- project/src/components/header/header.tsx
+++ project/src/components/header/header.tsx
@@ -1,28 +1,41 @@
+import { useAppSelector } from '../../hooks';
+import { AuthorizationStatus, AppRoute } from '../../const';
+import { Link } from 'react-router-dom';
+import Logo from '../logo/logo';
+
 function Header(): JSX.Element {
+  const authorizationStatus = useAppSelector((state) => state.authorizationStatus);
+
   return (
     <header className="header">
       <div className="container">
         <div className="header__wrapper">
           <div className="header__left">
-            <a className="header__logo-link header__logo-link--active" href="/">
-              <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41" />
-            </a>
+            <Logo type={'header'} width={81} height={41} />
           </div>
           <nav className="header__nav">
             <ul className="header__nav-list">
               <li className="header__nav-item user">
-                <a className="header__nav-link header__nav-link--profile" href="/">
+                <Link className="header__nav-link header__nav-link--profile" to={authorizationStatus === AuthorizationStatus.Auth ? AppRoute.Favorites : AppRoute.Login}>
                   <div className="header__avatar-wrapper user__avatar-wrapper">
                   </div>
-                  <span className="header__user-name user__name">Oliver.conner@gmail.com</span>
-                  <span className="header__favorite-count">3</span>
-                </a>
+                  {authorizationStatus === AuthorizationStatus.Auth
+                    ? (
+                      <>
+                        <span className="header__user-name user__name">Oliver.conner@gmail.com</span>
+                        <span className="header__favorite-count">3</span>
+                      </>
+                    )
+                    : (<span className="header__login">Sign in</span>)}
+                </Link>
               </li>
-              <li className="header__nav-item">
-                <a className="header__nav-link" href="/">
-                  <span className="header__signout">Sign out</span>
-                </a>
-              </li>
+              {authorizationStatus === AuthorizationStatus.Auth && (
+                <li className="header__nav-item">
+                  <a className="header__nav-link" href="/">
+                    <span className="header__signout">Sign out</span>
+                  </a>
+                </li>
+              )}
             </ul>
           </nav>
         </div>
