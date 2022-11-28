9994dec0fbd135b1cc32758b9dd9d467d1c3fc67
Oleh Parkhomenko <oleg.parkhom@gmail.com>
2022-11-28 00:58:58 -0700

    Определяет в глобальном состоянии приложения новое поле authorizationStatus

--- project/src/store/reducer.ts
+++ project/src/store/reducer.ts
@@ -1,5 +1,5 @@
 import { createReducer } from '@reduxjs/toolkit';
-import { CITIES } from '../const';
+import { AuthorizationStatus, CITIES } from '../const';
 import { changeCity, changeSortType, loadOffers, setOffersDataLoadingStatus } from './action';
 import { TypeOffersSort } from '../const';
 import { Hotel } from '../types/hotel';
@@ -9,13 +9,15 @@ type InitialState = {
   offers: Hotel[];
   sortType: string;
   isOffersDataLoading: boolean;
+  authorizationStatus: string;
 };
 
 const initialState: InitialState = {
   city: CITIES[3],
   offers: [] as Hotel[],
   sortType: TypeOffersSort.Default,
-  isOffersDataLoading: false
+  isOffersDataLoading: false,
+  authorizationStatus: AuthorizationStatus.Unknown,
 };
 
 const reducer = createReducer(initialState, (builder) => {
