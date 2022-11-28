aef3e7203cbd96a065c55d7ac343da81e404c5dd
Oleh Parkhomenko <oleg.parkhom@gmail.com>
2022-11-28 05:22:38 -0700

    Опишет новые асинхронные действия для авторизации

--- project/src/store/api-actions.ts
+++ project/src/store/api-actions.ts
@@ -5,6 +5,9 @@ import { Hotel } from '../types/hotel';
 import { loadOffers, requireAuthorization, setError, setOffersDataLoadingStatus } from './action';
 import { APIRoute, AuthorizationStatus, TIMEOUT_SHOW_ERROR } from '../const';
 import { store } from './';
+import { UserData } from '../types/user-data.js';
+import { AuthData } from '../types/auth-data.js';
+import { dropToken, saveToken } from '../services/token.js';
 
 export const clearErrorAction = createAsyncThunk(
   'data/clearError',
@@ -43,5 +46,31 @@ export const checkAuthAction = createAsyncThunk<void, undefined, {
     } catch {
       dispatch(requireAuthorization(AuthorizationStatus.NoAuth));
     }
+  },
+);
+
+export const loginAction = createAsyncThunk<void, AuthData, {
+  dispatch: AppDispatch;
+  state: State;
+  extra: AxiosInstance;
+}>(
+  'user/login',
+  async ({ login: email, password }, { dispatch, extra: api }) => {
+    const { data: { token } } = await api.post<UserData>(APIRoute.Login, { email, password });
+    saveToken(token);
+    dispatch(requireAuthorization(AuthorizationStatus.Auth));
+  },
+);
+
+export const logoutAction = createAsyncThunk<void, undefined, {
+  dispatch: AppDispatch;
+  state: State;
+  extra: AxiosInstance;
+}>(
+  'user/logout',
+  async (_arg, { dispatch, extra: api }) => {
+    await api.delete(APIRoute.Logout);
+    dropToken();
+    dispatch(requireAuthorization(AuthorizationStatus.NoAuth));
   },
 );
